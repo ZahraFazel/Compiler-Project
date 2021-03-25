13d37e969c58299cd5ed7bdb1c81092eba055ee0
fereshtah <fereshteh.forghani2012@gmail.com>
2021-03-25 14:40:24 +0430

    ID and KEYWORD identification added.

--- scanner.py
+++ scanner.py
@@ -8,11 +8,44 @@ spaces = [' ', '\n', '\t', '\v', '\r', '\f']
 
 class Scanner:
     def __init__(self, path):
-        self.line_num = 1
+        self.line = 1
         self.file = read(path)
-        self.unread_parts = self.file
+        self.unread = self.file
         self.errors, self.tokens = {}, {}
         self.symbol_table = set()
 
+    def return_function(self, token_type, token_len):
+        token = self.unread[:token_len]
+        self.unread = self.unread[token_len:]
+        return token_type, token
+
     def get_next_token(self):
-        pass
+        if len(self.unread) == 0:
+            return 'EOF', 'EOF'
+        # todo whitespace
+        # todo comment
+        # todo = and ==
+        # todo symbols
+        if self.unread[0].isalpha():
+            length = len(self.unread)
+            for i in range(len(self.unread)):
+                if not self.unread[i].isalnum():
+                    if self.unread[i] in symbols + spaces:
+                        length = i
+                        break
+                    else:
+                        return self.return_function('ERROR', i + 1)
+            if self.return_function[:length] in keywords:
+                return self.return_function('KEYWORD', length)
+            else:
+                return self.return_function('ID', length)
+        if self.unread[0].isnumeric():
+            for i in range(len(self.unread)):
+                if not self.unread[i].isnumeric():
+                    if self.unread[i] in symbols + spaces or self.unread[i].isalpha():
+                        return self.return_function('NUM', i)
+                    else:
+                        return self.return_function('ERROR', i + 1)
+        if self.unread[0] in spaces:
+            return self.return_function('WHITESPACE', 1)
+        return self.return_function('ERROR', 1)
