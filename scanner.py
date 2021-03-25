204e56659143cc9386458194246abb8f64e5c434
fereshtah <fereshteh.forghani2012@gmail.com>
2021-03-25 19:29:03 +0430

    fill errors function completed.

--- scanner.py
+++ scanner.py
@@ -19,7 +19,7 @@ class Scanner:
         self.fill_tokens(token_type, token)
         self.fill_errors(token_type, token)
         self.fill_symbol_table(token_type, token)
-        return
+        return token_type, token
 
     def get_next_token(self):
         if len(self.unread) == 0:
@@ -36,7 +36,7 @@ class Scanner:
                         length = i
                         break
                     else:
-                        return self.return_function('ERROR', i + 1)
+                        return self.return_function('INPUT ERROR', i + 1)
             if self.return_function[:length] in keywords:
                 return self.return_function('KEYWORD', length)
             else:
@@ -47,10 +47,10 @@ class Scanner:
                     if self.unread[i] in symbols + whitespaces or self.unread[i].isalpha():
                         return self.return_function('NUM', i)
                     else:
-                        return self.return_function('ERROR', i + 1)
+                        return self.return_function('NUM ERROR', i + 1)
         if self.unread[0] in whitespaces:
             return self.return_function('WHITESPACE', 1)
-        return self.return_function('ERROR', 1)
+        # return self.return_function('ERROR', 1)
 
     def fill_symbol_table(self, token_type, token):
         if token_type == 'KEYWORD':
@@ -65,12 +65,38 @@ class Scanner:
                 self.symbol_table['ID'] = set(token)
 
     def fill_errors(self, token_type, token):
-        pass
+        if token_type.endswith('ERROR'):
+            if self.line in self.errors.keys():
+                if token_type.startswith('NUM'):
+                    self.errors[self.line] += ' (' + token + ', Invalid number)'
+                elif token_type.startswith('INPUT'):
+                    self.errors[self.line] += ' (' + token + ', Invalid input)'
+                elif token_type.startswith('UNMATCHED'):
+                    self.errors[self.line] += ' (' + token + ', Unmatched comment)'
+                elif token_type.startswith('UNCLOSED'):
+                    self.errors[self.line] += ' (' + token + ', Unclosed comment)'
+            else:
+                if token_type.startswith('NUM'):
+                    self.errors[self.line] = ' (' + token + ', Invalid number)'
+                elif token_type.startswith('INPUT'):
+                    self.errors[self.line] = ' (' + token + ', Invalid input)'
+                elif token_type.startswith('UNMATCHED'):
+                    self.errors[self.line] = ' (' + token + ', Unmatched comment)'
+                elif token_type.startswith('UNCLOSED'):
+                    self.errors[self.line] = ' (' + token + ', Unclosed comment)'
 
     def fill_tokens(self, token_type, token):
-        if token_type == 'WHITESPACE' or token_type == 'COMMENT' or token_type == 'EOF':
-            return
-        if self.line in self.tokens.keys():
-            self.tokens[self.line] += ' (' + token_type + ', ' + token + ')'
-        else:
-            self.tokens[self.line] = ' (' + token_type + ', ' + token + ')'
+        if not token_type.endswith('ERROR'):
+            if token_type == 'WHITESPACE' or token_type == 'COMMENT' or token_type == 'EOF':
+                return
+            if self.line in self.tokens.keys():
+                self.tokens[self.line] += ' (' + token_type + ', ' + token + ')'
+            else:
+                self.tokens[self.line] = ' (' + token_type + ', ' + token + ')'
+
+    def scan(self):
+        while True:
+            token_type, token = self.get_next_token()
+            if token_type == 'EOF':
+                break
+        # todo write to files
