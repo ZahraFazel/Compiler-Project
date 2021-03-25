3edc550662ae316b9d3cdff0b016c920004d89a2
Zahra <zfazel1379@gmail.com>
2021-03-26 01:08:26 +0430

    Add Symbol identification Refactor previous commits

---
+++ compiler.py
@@ -0,0 +1,9 @@
+from scanner import Scanner
+
+scanner = Scanner('input_output/input1.txt')
+while True:
+    token_type = scanner.get_next_token()
+    if token_type[0] == 'EOF':
+        break
+print(scanner.symbol_table)
+print(scanner.tokens)
--- scanner.py
+++ scanner.py
@@ -1,17 +1,19 @@
 from utils import read
 
-# except for = and ==
-symbols = [';', ':', ',', '[', ']', '(', ')', '{', '}', '+', '-', '*', '<', '>']
-keywords = ['if', 'else', 'void', 'int', 'while', 'break', 'switch', 'default', 'case', 'return', 'for']
-whitespaces = [' ', '\n', '\t', '\v', '\r', '\f']
-
 
 class Scanner:
+
+    symbols = [';', ':', ',', '[', ']', '(', ')', '{', '}', '+', '-', '<', '>', '*', '=']
+    keywords = ['if', 'else', 'void', 'int', 'while', 'break', 'switch', 'default', 'case', 'return', 'for']
+    whitespaces = [' ', '\n', '\t', '\v', '\r', '\f']
+
     def __init__(self, path):
         self.line = 1
         self.file = read(path)
         self.unread = self.file
-        self.errors, self.tokens, self.symbol_table = {}, {}, {}
+        self.errors, self.tokens, self.symbol_table = {k: [] for k in range(self.file.count('\n') + 1)},\
+                                                      {k: [] for k in range(self.file.count('\n') + 1)}, \
+                                                      {'KEYWORD': set(), 'ID': set()}
 
     def return_function(self, token_type, token_len):
         token = self.unread[:token_len]
@@ -26,83 +28,52 @@ class Scanner:
             return 'EOF', 'EOF'
         # todo whitespace
         # todo comment
-        # todo = and ==
-        # todo symbols
+
+        if self.unread[0] in Scanner.symbols:
+            if len(self.unread) == 1:
+                return self.return_function('ERROR: Invalid input', 1)
+            else:
+                if self.unread[0:2] == '==':
+                    return self.return_function('SYMBOL', 2)
+                elif self.unread[0:2] == '*/':
+                    return self.return_function('ERROR: Unmatched comment', 2)
+                return self.return_function('SYMBOL', 1)
+
         if self.unread[0].isalpha():
             length = len(self.unread)
             for i in range(len(self.unread)):
                 if not self.unread[i].isalnum():
-                    if self.unread[i] in symbols + whitespaces:
+                    if self.unread[i] in Scanner.symbols + Scanner.whitespaces:
                         length = i
                         break
                     else:
-                        return self.return_function('INPUT ERROR', i + 1)
-            if self.unread[:length] in keywords:
+                        return self.return_function('ERROR: Invalid input', i + 1)
+            if self.unread[:length] in Scanner.keywords:
                 return self.return_function('KEYWORD', length)
             else:
                 return self.return_function('ID', length)
+
         if self.unread[0].isnumeric():
             for i in range(len(self.unread)):
                 if not self.unread[i].isnumeric():
-                    if self.unread[i] in symbols + whitespaces or self.unread[i].isalpha():
+                    if self.unread[i] in Scanner.symbols + Scanner.whitespaces or self.unread[i].isalpha():
                         return self.return_function('NUM', i)
                     else:
-                        return self.return_function('NUM ERROR', i + 1)
-        if self.unread[0] in whitespaces:
+                        return self.return_function('ERROR: Invalid number', i + 1)
+
+        if self.unread[0] in Scanner.whitespaces:
             return self.return_function('WHITESPACE', 1)
-        return self.return_function('ERROR', 1)
+
+        return self.return_function('ERROR: Invalid input', 1)
 
     def fill_symbol_table(self, token_type, token):
-        if token_type == 'KEYWORD':
-            if 'KEYWORD' in self.symbol_table.keys():
-                self.symbol_table['KEYWORD'].add(token)
-            else:
-                self.symbol_table['KEYWORD'] = set(token)
-        if token_type == 'ID':
-            if 'ID' in self.symbol_table.keys():
-                self.symbol_table['ID'].add(token)
-            else:
-                self.symbol_table['ID'] = set(token)
+        if token_type == 'ID' or token_type == 'KEYWORD':
+            self.symbol_table[token_type].add(token)
 
     def fill_errors(self, token_type, token):
-        if token_type.endswith('ERROR'):
-            if self.line in self.errors.keys():
-                if token_type.startswith('NUM'):
-                    self.errors[self.line] += ' (' + token + ', Invalid number)'
-                elif token_type.startswith('INPUT'):
-                    self.errors[self.line] += ' (' + token + ', Invalid input)'
-                elif token_type.startswith('UNMATCHED'):
-                    self.errors[self.line] += ' (' + token + ', Unmatched comment)'
-                elif token_type.startswith('UNCLOSED'):
-                    self.errors[self.line] += ' (' + token + ', Unclosed comment)'
-            else:
-                if token_type.startswith('NUM'):
-                    self.errors[self.line] = ' (' + token + ', Invalid number)'
-                elif token_type.startswith('INPUT'):
-                    self.errors[self.line] = ' (' + token + ', Invalid input)'
-                elif token_type.startswith('UNMATCHED'):
-                    self.errors[self.line] = ' (' + token + ', Unmatched comment)'
-                elif token_type.startswith('UNCLOSED'):
-                    self.errors[self.line] = ' (' + token + ', Unclosed comment)'
+        if token_type.startswith('ERROR'):
+            self.errors[self.line].append('(' + token + ',' + token_type[7:] + ')')
 
     def fill_tokens(self, token_type, token):
-        if not token_type.endswith('ERROR'):
-            if token_type == 'WHITESPACE' or token_type == 'COMMENT' or token_type == 'EOF':
-                return
-            if self.line in self.tokens.keys():
-                self.tokens[self.line] += ' (' + token_type + ', ' + token + ')'
-            else:
-                self.tokens[self.line] = ' (' + token_type + ', ' + token + ')'
-
-    def scan(self):
-        while True:
-            token_type = self.get_next_token()
-            if token_type[0] == 'EOF':
-                break
-        # todo write to files
-
-
-scanner = Scanner('input_output/input1.txt')
-scanner.scan()
-print(scanner.symbol_table)
-print(scanner.tokens)
+        if not token_type.startswith('ERROR') and (token_type == 'ID' or token_type == 'KEYWORD' or token_type == 'NUM'):
+            self.tokens[self.line].append('(' + token_type + ', ' + token + ')')
